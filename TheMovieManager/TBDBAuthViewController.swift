9595767ed559924bc2a26ad252d7628699a55255
Chris Archibald <carchi8py@gmail.com>
2015-07-03 10:42:31 -0700

    Got Authentication to work

--- TheMovieManager/TBDBAuthViewController.swift
+++ TheMovieManager/TBDBAuthViewController.swift
@@ -40,6 +40,14 @@ class TMDBAuthViewController: UIViewController, UIWebViewDelegate {
     
     // TODO: Add a function to complete Step 2a
     
+    func webViewDidFinishLoad(webView: UIWebView) {
+        if (webView.request!.URL?.absoluteString! == "\(TMDBClient.Constants.AuthorizationURL)\(requestToken!)/allow") {
+            self.dismissViewControllerAnimated(true, completion: { () -> Void in
+                self.completionHandler!(success: true, errorString: nil)
+            })
+        }
+    }
+    
     func cancelAuth() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
